9b0ed4ae983588e4be7b3d0713de3316653a1017
Chooton838 <choton838@gmail.com>
2022-10-15 23:14:27 +0600

    GitAction Testing

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -17,64 +17,64 @@ test("Login & Getting Dashboard Overview Details ", async ({ page }) => {
     await dashboard.overview_details();
 });
 
-/* ------------------------ Plugin ------------------------ */
-test("Plugin Create & Details @fast", async ({ page }) => {
-    const plugin = new PluginPage(page);
+// /* ------------------------ Plugin ------------------------ */
+// test("Plugin Create & Details @fast", async ({ page }) => {
+//     const plugin = new PluginPage(page);
 
-    await plugin.free_plugin_create();
-    // await plugin.pro_plugin_create(data.plugin_data.website_url, data.plugin_data.product_name);
+//     await plugin.free_plugin_create();
+//     // await plugin.pro_plugin_create(data.plugin_data.website_url, data.plugin_data.product_name);
 
-    /* -------- Plugin Update -------- */
-    /**
-     * plugin_name = Any valid plugin name and this plugin will be updated
-     * data.plugin_data = this "plugin_name" plugin will be updated with this new data
-     */
+//     /* -------- Plugin Update -------- */
+//     /**
+//      * plugin_name = Any valid plugin name and this plugin will be updated
+//      * data.plugin_data = this "plugin_name" plugin will be updated with this new data
+//      */
 
-    let plugin_name: string = "Automated Plugin"; //Any valid updateable plugin name
-    //await plugin.plugin_update(data.plugin_data, plugin_name);
-});
+//     let plugin_name: string = "Automated Plugin"; //Any valid updateable plugin name
+//     //await plugin.plugin_update(data.plugin_data, plugin_name);
+// });
 
-/* ------------------------ Theme ------------------------ */
-test("Theme", async ({ page }) => {
-    const theme = new ThemePage(page);
+// /* ------------------------ Theme ------------------------ */
+// test("Theme", async ({ page }) => {
+//     const theme = new ThemePage(page);
 
-    theme.free_theme_create();
-    theme.pro_theme_create(
-        data.theme_data.website_url,
-        data.theme_data.product_name
-    );
+//     theme.free_theme_create();
+//     theme.pro_theme_create(
+//         data.theme_data.website_url,
+//         data.theme_data.product_name
+//     );
 
-    /* -------- Theme Update -------- */
-    /**
-     * theme_name = Any valid theme name and this theme will be updated
-     * data.theme_data = this "theme_name" theme will be updated with this new data
-     */
+//     /* -------- Theme Update -------- */
+//     /**
+//      * theme_name = Any valid theme name and this theme will be updated
+//      * data.theme_data = this "theme_name" theme will be updated with this new data
+//      */
 
-    const pluginpage = new PluginPage(page);
-    let theme_name: string = "Automated Theme 2";
-    await theme.theme_update(data.theme_data, theme_name);
-});
+//     const pluginpage = new PluginPage(page);
+//     let theme_name: string = "Automated Theme 2";
+//     await theme.theme_update(data.theme_data, theme_name);
+// });
 
-/* ------------------------ Bundle ------------------------ */
-test("Bundle", async ({ page }) => {
-    const bundle = new BundlePage(page);
+// /* ------------------------ Bundle ------------------------ */
+// test("Bundle", async ({ page }) => {
+//     const bundle = new BundlePage(page);
 
-    let plugin_name: string = "Automated Plugin 2";
-    await bundle.bundle_create(data.bundle_data);
+//     let plugin_name: string = "Automated Plugin 2";
+//     await bundle.bundle_create(data.bundle_data);
 
-    /* -------- Bundle Update -------- */
-    /**
-     * bundle_name = Any valid bundle name and this bundle will be updated
-     * data.bundle_data = this "bundle_name" bundle will be updated with this new data
-     */
+//     /* -------- Bundle Update -------- */
+//     /**
+//      * bundle_name = Any valid bundle name and this bundle will be updated
+//      * data.bundle_data = this "bundle_name" bundle will be updated with this new data
+//      */
 
-    let bundle_name: string = ""; //Any valid updateable bundle name
-    await bundle.bundle_update(data.bundle_data, bundle_name);
-});
+//     let bundle_name: string = ""; //Any valid updateable bundle name
+//     await bundle.bundle_update(data.bundle_data, bundle_name);
+// });
 
-/* ------------------------ Products Delete ------------------------ */
-test("Prodcut Delete", async ({ page }) => {
-    const product = new ProductPage(page);
-    let product_name: string = data.plugin_data.plugin_name; // Product Name could be any valid Plugin/Theme/Bundle name;
-    product.product_delete(product_name);
-});
+// /* ------------------------ Products Delete ------------------------ */
+// test("Prodcut Delete", async ({ page }) => {
+//     const product = new ProductPage(page);
+//     let product_name: string = data.plugin_data.plugin_name; // Product Name could be any valid Plugin/Theme/Bundle name;
+//     product.product_delete(product_name);
+// });
